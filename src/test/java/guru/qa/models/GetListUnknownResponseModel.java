d65a04849bc0b87b189a2525d7af7ca6ffd1ed43
EphimSh <ephimizm@gmail.com>
2023-07-29 11:36:28 +0300

    add GetListUnknownResponseModel.java

---
+++ src/test/java/guru/qa/models/GetListUnknownResponseModel.java
@@ -0,0 +1,22 @@
+package guru.qa.models;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class GetListUnknownResponseModel {
+
+    int page, per_page, total, total_pages;
+
+    List<UserListData> data;
+
+    Support support;
+    @Data
+    public static class UserListData{
+        int id,year;
+        String name,color,pantone_value;
+    }
+    @Data
+    public static class Support{
+        String url, text;
+    }
+}
