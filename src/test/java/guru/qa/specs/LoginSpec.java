bdc8f9aa6a0e1c7185d982dfdd5cefdd5a650466
EphimSh <ephimizm@gmail.com>
2023-07-29 11:36:50 +0300

    add LoginSpec.java

---
+++ src/test/java/guru/qa/specs/LoginSpec.java
@@ -0,0 +1,32 @@
+package guru.qa.specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static guru.qa.helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class LoginSpec {
+
+    public static RequestSpecification loginRequestSpec = with()
+            .log().uri()
+            .log().method()
+            .log().body()
+            .filter(withCustomTemplates())
+            .contentType(ContentType.JSON)
+            .baseUri("https://reqres.in")
+            .basePath("/api");
+
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody(matchesJsonSchemaInClasspath("schema/successful-login-response-schema.json"))
+            .build();
+
+
+}
