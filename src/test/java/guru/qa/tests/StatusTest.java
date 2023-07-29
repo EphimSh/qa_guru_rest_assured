99cd0d241559d820f32284a1a280ad5c6a32e081
EphimSh <ephimizm@gmail.com>
2023-07-29 11:34:34 +0300

    update StatusTest.java

--- src/test/java/guru/qa/tests/StatusTest.java
+++ src/test/java/guru/qa/tests/StatusTest.java
@@ -1,135 +1,122 @@
 package guru.qa.tests;
 
-import guru.qa.models.LoginBodyLombokModel;
-import guru.qa.models.LoginBodyPojoModel;
-import guru.qa.models.LoginResponseLombokModel;
-import guru.qa.models.LoginResponsePojoModel;
-import io.restassured.http.ContentType;
+import guru.qa.models.*;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+
+import static guru.qa.specs.GetUserListSpec.*;
+import static guru.qa.specs.LoginSpec.*;
+import static guru.qa.specs.UserSpec.*;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StatusTest extends TestBase{
+@Tag("api")
+@Owner("EphimSh")
+@Feature("api test practice")
+public class StatusTest {
+
 
     @Test
-    void deleteUser(){
-        given()
-                .log().uri()
-                .log().body()
-                .log().method()
-                .when()
-                .delete("/users/3")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(204);
-    }
-
-    @Test
-    void patchUser(){
-        String data = "{\n" +
-                "    \"name\": \"morpheus\",\n" +
-                "    \"job\": \"zion resident\"\n" +
-                "}";
-        given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .when()
-                .contentType(ContentType.JSON)
-                .body(data)
-                .patch("/users/1")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body("job", is("zion resident"));
-    }
-
-    @Test
-    void successfulLogin(){
-
-        LoginBodyPojoModel authData = new LoginBodyPojoModel();
+    @DisplayName("successful login")
+    void successfulLogin() {
+        LoginRequestModel authData = new LoginRequestModel();
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("cityslicka");
 
-        LoginResponsePojoModel loginResponse = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(ContentType.JSON)
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponsePojoModel.class);
-
-        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+        LoginResponseModel loginResponse = step("make a request", () ->
+                given(loginRequestSpec)
+                        .body(authData)
+                        .when()
+                        .post("/login")
+                        .then()
+                        .spec(loginResponseSpec)
+                        .extract().as(LoginResponseModel.class));
+        step("check the request", () -> {
+            assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+        });
     }
 
     @Test
-    void successfulLogin1(){
+    @DisplayName("get list of unknown objects")
+    void getListUnknown() {
+        GetListUnknownResponseModel getListResponse = step("make a request", () ->
+                given(getUserListSpec)
+                        .when()
+                        .get("/unknown")
+                        .then()
+                        .spec(getUserListResponseSpec)
+                        .extract().as(GetListUnknownResponseModel.class)
+        );
 
-        LoginBodyLombokModel authData = new LoginBodyLombokModel();
-        authData.setEmail("eve.holt@reqres.in");
-        authData.setPassword("cityslicka");
-
-        LoginResponseLombokModel loginResponse = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(ContentType.JSON)
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponseLombokModel.class);
-
-        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+        step("check request", () -> {
+            assertEquals(getListResponse.getData().size(), 6);
+        });
     }
 
     @Test
-    void successfulRegister(){
-        String data = "{\n" +
-                "    \"email\": \"eve.holt@reqres.in\",\n" +
-                "    \"password\": \"pistol\"\n" +
-                "}";
+    @DisplayName("aqua sky is in unknown objects list")
+    void getColorFromListUnknown() {
+        GetListUnknownResponseModel getListResponse = step("make a request", () ->
+                given(getUserListSpec)
+                        .when()
+                        .get("/unknown")
+                        .then()
+                        .spec(getUserListResponseSpec)
+                        .extract().as(GetListUnknownResponseModel.class)
+        );
 
-        given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(ContentType.JSON)
-                .body(data)
-                .when()
-                .post("/register")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+        step("check request", () -> {
+            assertEquals(getListResponse.getData().get(3).getName(), "aqua sky");
+        });
     }
 
     @Test
-    void getListUsersSize(){
-        given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .when()
-                .get("/users?page=2")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body("data", hasSize(6));
+    @DisplayName("successful user create")
+    void postCreateUser() {
+        UserRequestModel newUser = new UserRequestModel();
+        newUser.setName("Vlad Tepes");
+        newUser.setJob("The Impaler");
+
+        UserResponseModel createUserResponse = step("make a request", () ->
+                given(createUserSpec)
+                        .body(newUser)
+                        .when()
+                        .post("/users")
+                        .then()
+                        .spec(createUserResponseSpec)
+                        .extract().as(UserResponseModel.class)
+        );
+
+        step("check request", () -> {
+            assertEquals("The Impaler", createUserResponse.getJob());
+        });
     }
+
+    @Test
+    @DisplayName("successful user update")
+    void patchCreateUser() {
+        UserRequestModel newUser = new UserRequestModel();
+        newUser.setName("Vlad Tepes");
+        newUser.setJob("The Impaler");
+
+        UserResponseModel createUserResponse = step("make a request", () ->
+                given(createUserSpec)
+                        .body(newUser)
+                        .when()
+                        .patch("/users/2")
+                        .then()
+                        .spec(patchUserResponseSpec)
+                        .extract().as(UserResponseModel.class)
+        );
+
+        step("check request", () -> {
+            assertEquals("The Impaler", createUserResponse.getJob());
+        });
+    }
+
 }
