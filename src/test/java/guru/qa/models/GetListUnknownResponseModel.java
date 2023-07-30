d276b479934861dec96bf51d19bde4f1bd8f6a2d
EphimSh <ephimizm@gmail.com>
2023-07-30 11:10:49 +0300

    update GetListUnknownResponseModel.java

--- src/test/java/guru/qa/models/GetListUnknownResponseModel.java
+++ src/test/java/guru/qa/models/GetListUnknownResponseModel.java
@@ -1,5 +1,6 @@
 package guru.qa.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -7,7 +8,12 @@ import java.util.List;
 @Data
 public class GetListUnknownResponseModel {
 
-    int page, per_page, total, total_pages;
+    int page, total;
+
+    @JsonProperty("per_page")
+    int perPage;
+    @JsonProperty("total_pages")
+    int totalPages;
 
     List<UserListData> data;
 
